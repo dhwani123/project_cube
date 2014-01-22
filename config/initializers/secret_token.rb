657eda946a973ae2476029ed96244d965706e0b9
Dhwani Bhatt <dhwanibhatt3007@gmail.com>
2014-01-22 17:03:45 +0530

    done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectCube::Application.config.secret_token = '477edb9b49b81f8c2467c1347b97568da2e155a9edadbb2e9809667d87b4ee519bf6cd352cb3e1b35b2079d5fe9dbb8a4dccc573d1e0c7ddf880845e0a8b2902'
